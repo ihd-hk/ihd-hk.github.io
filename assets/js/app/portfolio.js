81cf4fec57beb4229185b46af992c1716143bc0c
Nicholas Thompson <nick@fullfatthings.com>
2019-04-16 00:38:55 +0100

    Reworking the Projects section

--- assets/js/app/portfolio.js
+++ assets/js/app/portfolio.js
@@ -1,36 +1,41 @@
 requirejs(['jquery', 'shuffle', 'select2', 'underscore'], function ($, Shuffle, select2, _) {
   'use strict';
   var $list = $('#porfolio_grid'),
-    $locationFilter = $('#portfolio_filters select[name="location"]'),
-    $disciplineFilter = $('#portfolio_filters select[name="discipline"]'),
-    $typeFilter = $('#portfolio_filters select[name="type"]');
+      $locationFilter = $('#portfolio_filters select[name="location"]'),
+      $disciplineFilter = $('#portfolio_filters select[name="discipline"]'),
+      $typeFilter = $('#portfolio_filters select[name="type"]');
 
-  // $('#portfolio_filters').sticky({topSpacing: 50});
+  var filterData = {
+    locations: {},
+    disciplines: {},
+    types: {},
+  };
 
-
-
-  // @TODO - figure out how to deal with the Chinese side of this.
-  // var kvMapper = function(object) {
-  //   return Object.keys(object).map(function(key) {
-  //     return { id: key, text: object[key][portfolio_conf.lang] };
-  //   });
-  // };
+  $list.find('.project').each(function(i, el) {
+    ['locations', 'disciplines', 'types'].forEach(function(facet) {
+      el.dataset[facet].split(',').forEach(function(value) {
+        if (filterData[facet][value] === undefined) {
+          filterData[facet][value] = { id: value, text: value }
+        }
+      })
+    })
+  });
 
   $locationFilter.select2({
-    // data: kvMapper(data.countries),
+    data: Object.values(filterData.locations).sort(function(a, b) { return a.text > b.text }),
     placeholder: 'All Countries',
     minimumResultsForSearch: Infinity,
     theme: 'classic'
   });
 
   $disciplineFilter.select2({
-    // data: kvMapper(data.disciplines),
+    data: Object.values(filterData.disciplines).sort(function(a, b) { return a.text > b.text }),
     placeholder: 'All Disciplines',
     minimumResultsForSearch: Infinity,
     theme: 'classic'
   });
   $typeFilter.select2({
-    // data: kvMapper(data.codes),
+    data: Object.values(filterData.types).sort(function(a, b) { return a.text > b.text }),
     placeholder: 'All Types',
     minimumResultsForSearch: Infinity,
     theme: 'classic'
@@ -60,7 +65,7 @@ requirejs(['jquery', 'shuffle', 'select2', 'underscore'], function ($, Shuffle, 
   // });
 
   var shuffle = new Shuffle($list[0], {
-    itemSelector: '.portfolio-item'
+    itemSelector: '.project'
   });
 
 
@@ -81,11 +86,10 @@ requirejs(['jquery', 'shuffle', 'select2', 'underscore'], function ($, Shuffle, 
     }
 
     if (selectedLocations.length + selectedDisciplines.length + selectedTypes.length > 0 ) {
-      shuffle.filter(function(element) {
-        var $el = $(element);
-        return (selectedLocations   === null ||  _.intersection($el.data('locations'  ), selectedLocations  ).length === selectedLocations.length) &&
-               (selectedDisciplines === null ||  _.intersection($el.data('disciplines'), selectedDisciplines).length === selectedDisciplines.length) &&
-               (selectedTypes       === null ||  _.intersection($el.data('codes'      ), selectedTypes      ).length === selectedTypes.length);
+      shuffle.filter(function(el) {
+        return (selectedLocations   === null ||  _.intersection(el.dataset['locations'  ].split(','), selectedLocations  ).length === selectedLocations.length) &&
+               (selectedDisciplines === null ||  _.intersection(el.dataset['disciplines'].split(','), selectedDisciplines).length === selectedDisciplines.length) &&
+               (selectedTypes       === null ||  _.intersection(el.dataset['types'      ].split(','), selectedTypes      ).length === selectedTypes.length);
       });
     }
     else {
@@ -93,6 +97,6 @@ requirejs(['jquery', 'shuffle', 'select2', 'underscore'], function ($, Shuffle, 
     }
   };
 
-  $('#portfolio_filters ').on('change', 'select', updateFilter);
+  $('#portfolio_filters').on('change', 'select', updateFilter);
 
 });
